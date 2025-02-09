efcd676fba9683133298adbc72951d9a00ad09c0
Fayvira <46143024+Fayvira-DraGon@users.noreply.github.com>
2025-02-08 23:03:54 -0500

    improved gradle build, properties, & wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("dev.architectury.loom") version("1.7-SNAPSHOT")
+    id("dev.architectury.loom") version("1.9-SNAPSHOT")
     id("architectury-plugin") version("3.4-SNAPSHOT")
     kotlin("jvm") version "2.0.21"
 }
@@ -14,43 +14,34 @@ architectury {
 }
 
 loom {
-    silentMojangMappingsLicense()
-
     mixin {
-        defaultRefmapName.set("mixins.${project.name}.refmap.json")
+        defaultRefmapName.set("${project.name}.refmap.json")
+    }
+    runs {
+        named("client") {
+            property("devauth.enabled", "true") // enable devauth, & on first run generate config & login link in log
+            property("devauth.account", "main") // account type: minecraft
+        }
     }
 }
 
 repositories {
     mavenCentral()
-    maven("https://dl.cloudsmith.io/public/geckolib3/geckolib/maven/")
-    maven("https://maven.impactdev.net/repository/development/")
-    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://maven.impactdev.net/repository/development/") // cobblemon
+    maven("https://pkgs.dev.azure.com/djtheredstoner/DevAuth/_packaging/public/maven/v1") // devauth
 }
 
 dependencies {
-    minecraft("net.minecraft:minecraft:${project.property("minecraft_version")}")
-    mappings("net.fabricmc:yarn:${project.property("yarn_mappings")}")
-    modImplementation("net.fabricmc:fabric-loader:${project.property("fabric_loader_version")}")
-
-    modRuntimeOnly("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_api_version")}")
-    modImplementation(fabricApi.module("${project.property("fabric_command_api_version")}", "${project.property("fabric_api_version")}"))
-
-    modImplementation("net.fabricmc:fabric-language-kotlin:${project.property("kotlin_version")}")
-    modImplementation("com.cobblemon:fabric:${project.property("cobblemon_version")}")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:${project.property("junit-jupiter-api_version")}")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${project.property("junit-jupiter-engine_version")}")
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+    minecraft("net.minecraft:minecraft:${project.property("minecraft_version")}") // minecraft
+    mappings("net.fabricmc:yarn:${project.property("yarn_mappings")}") // mappings
+    modImplementation("net.fabricmc:fabric-loader:${project.property("fabric_loader_version")}") // mod loader
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_api_version")}") // fabric api
+    modImplementation("net.fabricmc:fabric-language-kotlin:${project.property("kotlin_version")}") // kotlin
+    modImplementation("com.cobblemon:fabric:${project.property("cobblemon_version")}") // cobblemon
+    modRuntimeOnly("me.djtheredstoner:DevAuth-fabric:1.2.1") // devauth
 }
 
 tasks.processResources {
     inputs.property("version", project.version)
-
-    filesMatching("fabric.mod.json") {
-        expand(project.properties)
-    }
+    filesMatching("fabric.mod.json") { expand(project.properties) }
 }
